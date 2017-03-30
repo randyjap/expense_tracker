e42c2d6233faeca18c63c5ece956c01f1ec7019e
randyjap <randyjap@gmail.com>
2017-03-30 14:12:11 -0700

    remove store from window

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -15,6 +15,4 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     const root = document.getElementById('root');
     ReactDOM.render(<Root store={ store }/>,root);
-    //TODO
-    window.store = store;
 });
