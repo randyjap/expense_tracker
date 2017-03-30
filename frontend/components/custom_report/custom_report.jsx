84f1ed0bef4b5e8983ce004b2620ddf59f12f3d7
randyjap <randyjap@gmail.com>
2017-03-30 13:48:18 -0700

    add custom range report

--- frontend/components/custom_report/custom_report.jsx
+++ frontend/components/custom_report/custom_report.jsx
@@ -5,19 +5,78 @@ import { values } from 'lodash';
 class CustomReport extends React.Component {
   constructor(props) {
     super(props);
+    this.renderCustomRangeReport = this.renderCustomRangeReport.bind(this);
+
+    this.state = {
+      startDate: "",
+      endDate: ""
+    };
+  }
+
+  componentDidMount(){
+    this.props.fetchAllExpenses();
+  }
+
+  logChange(field) {
+    return e => {
+      this.setState({
+        [field]: e.target.value
+      });
+      console.log(this.state);
+    };
+  }
+
+  renderCustomRangeReport(){
+    let data;
+    if (this.state.startDate === "" || this.state.endDate === "") {
+      data = values(this.props.expenses);
+    } else {
+      data = (
+        values(this.props.expenses)
+          .filter( expense => (
+            moment(expense.date)
+              .isBetween(this.state.startDate, this.state.endDate)
+      )));
+    }
+    let expenseItems;
+    if (data) {
+      expenseItems = data.map(item => {
+        return (
+          <tr>
+            <td>{item.user_id}</td>
+            <td>{item.id}</td>
+            <td>{item.date}</td>
+            <td>{item.amount}</td>
+            <td>{item.description}</td>
+          </tr>
+        );
+      });
+    }
+    return expenseItems;
   }
 
   render() {
     return (
       <div>
-        <h1>Weekly Totals</h1>
-        <table>
-          <tr>
-            <th>Year</th>
-            <th>Week Number</th>
-            <th>Total</th>
-          </tr>
-        </table>
+        <h1>Custom Range Report</h1>
+        <input
+           type='date'
+           value={this.state.startDate}
+           onChange={this.logChange("startDate")} />
+        <input
+          type='date'
+          value={this.state.endDate}
+          onChange={this.logChange("endDate")} />
+          <table>
+            <tr>
+             <th>User ID</th>
+             <th>Expense ID</th>
+             <th>Date</th>
+             <th>Amount</th>
+             <th>Description</th>
+           </tr>
+            {this.renderCustomRangeReport()}
+          </table>
       </div>
     );
   }
--- frontend/components/custom_report/custom_report_container.jsx
+++ frontend/components/custom_report/custom_report_container.jsx
@@ -1,11 +1,14 @@
 import { connect } from 'react-redux';
 import CustomReport from './custom_report';
+import { fetchAllExpenses } from '../../actions/expenses_actions';
 
 const mapStateToProps = state => ({
-  currentUser: state.session.currentUser
+  currentUser: state.session.currentUser,
+  expenses: state.expenses.expenses
 });
 
 const mapDispatchToProps = dispatch => ({
+  fetchAllExpenses: () => dispatch(fetchAllExpenses())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CustomReport);
